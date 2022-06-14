531e14cd79992851ac2a9c8e13c6f60279e4814b
JustinLaurenso <justin.laurenso166@gmail.com>
2022-06-14 16:29:05 +0700

    Push todolist

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import {Routes, Route, Link} from "react-router-dom"
+import Home from "../src/views/Home"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+
+export default function Header() {
+
+    return (
+        <div>
+            <header className="bg-blue-500 px-12 py-8">
+                <nav className="flex items-center justify-between">
+                    <div className="text-white text-2xl font-bold tracking-wider">Simple To Do List React</div>
+                    <div className="flex gap-10 text-white font-semibold tracking-wide text-lg">
+                        <span>By Justin Laurenso</span>
+                    </div>
+                </nav>
+            </header>
+        </div>
+    )
+}
