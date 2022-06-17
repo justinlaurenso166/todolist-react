a232430ae62c7736cade5ab521f35e616e10245d
JustinLaurenso <justin.laurenso166@gmail.com>
2022-06-17 22:52:23 +0700

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
         <div>
             <header className="bg-blue-500 lg:px-12 lg:py-8 px-5 py-6">
                 <nav className="flex items-center justify-between">
-                    <a href={"/todolist-react"}>
+                    <a href={"/todolist-react/"}>
                         <div className="text-white text-lg lg:text-2xl font-bold tracking-wider">Simple To Do List React</div>
                     </a>
                     <div className="flex gap-10 text-white font-semibold tracking-wide lg:text-lg text-md text-right">
